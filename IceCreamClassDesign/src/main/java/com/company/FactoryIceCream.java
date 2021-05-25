0adf9b44fc2da59af2c02e747dd8c3904f10fbe8
elccastrovowiqu <zhouhuiqi380330@126.com>
2021-05-26 02:53:43 +0800

    added ice cream file

---
+++ IceCreamClassDesign/src/main/java/com/company/FactoryIceCream.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class FactoryIceCream {
+
+    private int productId;
+    private String productName;
+    private int packagingId;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getPackagingId() {
+        return packagingId;
+    }
+
+    public void setPackagingId(int packagingId) {
+        this.packagingId = packagingId;
+    }
+
+
+}
---
+++ IceCreamClassDesign/src/main/java/com/company/StoreIceCream.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class StoreIceCream {
+
+
+    private String flavor;
+    private String color;
+    private int price;
+
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+}
